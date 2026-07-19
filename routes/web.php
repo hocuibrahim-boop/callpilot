467e2956d1eb9615af5e0d9d9b90874e4f6d3ec9
Deploy <deploy@callpilot.com>
2026-07-19 09:51:23 +0000

    feat: tanitim sitesi + demo + APK indirme
    - / adresinde tanitim sitesi (canli telefon akisi, kaybolan numaralar)
    - /demo etkilesimli prototip
    - /indir/CallPilot.apk uygulama indirme
    - /durum servis saglik yaniti

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,26 @@
 
 use Illuminate\Support\Facades\Route;
 
+// Tanitim sitesi
 Route::get('/', function () {
+    return response()->file(public_path('site/index.html'));
+});
+
+// Etkilesimli demo
+Route::get('/demo', function () {
+    return response()->file(public_path('site/demo.html'));
+});
+
+// Uygulama indirme
+Route::get('/indir/CallPilot.apk', function () {
+    $yol = public_path('indir/CallPilot.apk');
+    abort_unless(file_exists($yol), 404);
+    return response()->download($yol, 'CallPilot.apk', [
+        'Content-Type' => 'application/vnd.android.package-archive',
+    ]);
+});
+
+// Servis durumu (eski ana sayfa yaniti)
+Route::get('/durum', function () {
     return response()->json(['app' => 'CallPilot', 'status' => 'ok', 'version' => '1.0']);
 });
